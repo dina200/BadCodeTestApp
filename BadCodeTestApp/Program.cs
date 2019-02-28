248915ccb8214aeaa1f0e516c719f056968a4758
dina200 <dinapeace@ya.ru>
2019-02-28 15:25:43 +0200

    IndexOutOfRangeException for args

--- BadCodeTestApp/Program.cs
+++ BadCodeTestApp/Program.cs
@@ -1,18 +1,30 @@
 ﻿using System;
+using System.Collections;
 using System.Linq;
 using System.Reflection;
 using System.Text.RegularExpressions;
 using BadCodeTestApp.Commands;
+using NLog;
+using static System.String;
 
 namespace BadCodeTestApp
 {
     class Program
     {
-
+        private static string _command;
+        private static string _param;
         public static void Main(string[] args)
         {
-            string command = args[0];
-            string param = args[1];
+            try
+            {
+               _command = args[0];
+               _param = args[1];
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("The params (args[0], args[1]) can't be empty, try again...");
+                return;
+            }
             
             var commands = from type in Assembly.GetExecutingAssembly().GetTypes()
                 where type.GetInterfaces().Contains(typeof(ICommand))
@@ -21,13 +33,10 @@ namespace BadCodeTestApp
 
             foreach (var c in commands)
             {
-                if (Regex.IsMatch(command, c.GetCommandPattern(), RegexOptions.IgnoreCase))
-                {
-                    c.Execute(command, param);
-                    break;
-                }
+                if (!Regex.IsMatch(_command, c.GetCommandPattern(), RegexOptions.IgnoreCase)) continue;
+                c.Execute(_command, _param);
+                break;
             }
-            
         }
     }
 }
